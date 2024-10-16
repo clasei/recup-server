67fa078456fca859d4612e19f1621cebf42e193b
clara seijo <clasei@proton.me>
2024-10-16 13:04:33 +0200

    structure + routes initial setup

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config()
+// require("./db") // waiting for database
+
+const express = require("express")
+const app = express()
+
+// require("./config")(app) // waiting for config
+
+const indexRoutes = require("./routes/index.routes")
+app.use("/api", indexRoutes)
+
+// require("./error-handling")(app) // waiting for error-handling
+
+// // TEST
+// app.get("/", (req, res) => {
+//   res.send(`hey there this is rec-up-back-end`)
+// })
+
+
+module.exports = app
